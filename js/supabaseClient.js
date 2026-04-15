4890bb9e3f27c119f7f5f6ad65eb9f103a8ecacd
kevin <kevin.rojokevin17@gmail.com>
2026-04-15 20:30:28 -0300

    Agregar funcionalidad para mostrar/ocultar contraseña en el formulario de inicio de sesión y mejorar el estilo del campo de contraseña

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -4,4 +4,9 @@ const supabaseUrl = "https://xaplplzqxhyjjcebhtvz.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhcGxwbHpxeGh5ampjZWJodHZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxMjExNjAsImV4cCI6MjA4NDY5NzE2MH0.T5SNqohHrSPZiMLPt_b2RzFZdGiGxTqddHiWKG1aIEk";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    storage: sessionStorage,
+  },
+});
